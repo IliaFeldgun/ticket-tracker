5883760999d02134d9801cc80f5299d8bdf3b811
Ilia Feldgun <iliafeldgun@outlook.com>
2020-03-05 18:36:16 +0200

    Added stub for table view

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,3 +1,10 @@
 import React from 'react';
 
 // TODO impelment devextreme here
+export class TicketTableView extends React.PureComponent{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}

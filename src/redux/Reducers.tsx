50616794587d92e7e66eff1fd0d039af9ed75c0f
Ilia Feldgun <iliafeldgun@outlook.com>
2020-03-06 19:32:09 +0200

    Added actions to reducer

--- src/redux/Reducers.tsx
+++ src/redux/Reducers.tsx
@@ -1,19 +1,36 @@
 import * as Actions from './Actions'
-import { TicketListActionTypes, ITicketList } from './Types'
+import { TicketListActionTypes, 
+         ITicketList, 
+         CREATE_TICKET, 
+         UPDATE_TICKET, 
+         DELETE_TICKET, 
+         FILTER_BY } from './Types'
+import { ITicket } from '../models/Ticket'
 
 const initialState : ITicketList = {
-    ticketList: []
+    ticketList: [],
+    filter: ""
 }
 
 function ticketTracker(state = initialState, 
                        action : TicketListActionTypes) : ITicketList
 {
-    
     switch (action.type) {
         case CREATE_TICKET:
-            return {}
+            return Object.assign({}, state, { 
+                ticketList: [...state.ticketList, action.ticket], filter: state.filter})
+        case UPDATE_TICKET:
+            return Object.assign({}, state, { 
+                ticketList: [], filter: state.filter})
+        case DELETE_TICKET:
+            return Object.assign({}, state, { 
+                ticketList: state.ticketList.filter(ticket => {
+                                 ticket.ticketId === action.ticketId})
+                })
+        case FILTER_BY: // TODO: maybe will be managed here??
+            return Object.assign({}, state, {filter: action.filter})
+        default:
+            return state
     }
-
-    return state;
 }
 
